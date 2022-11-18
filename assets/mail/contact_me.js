f335af20bef75e8d3955685b614de15651b4840d
ThyAlpha001 <101531499+ThyAlpha001@users.noreply.github.com>
2022-11-18 04:59:02 +0300

    Added API

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://k19v6vppl2.execute-api.eu-west-1.amazonaws.com/default/cc4-lambda-function",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
